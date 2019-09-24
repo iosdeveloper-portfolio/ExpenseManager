1cee374d7f48b7a2ef17ddc4a24f2b60f8bce3a6
Nishant <nishant@github.io>
2019-09-24 16:42:42 +0530

    Initial project setup

---
+++ ExpenseManager/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExpenseManager
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
